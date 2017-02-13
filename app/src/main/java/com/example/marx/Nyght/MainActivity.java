ccae5ec3f18813407ab77dd17308c73706bfcdfd
marxlow <marx_low94@hotmail.com>
2017-02-13 23:08:45 +0800

    Create 4 new fragments xml and imported their menu icons

--- app/src/main/java/com/example/marx/Nyght/MainActivity.java
+++ app/src/main/java/com/example/marx/Nyght/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.marx.Nyght;
 
 import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import com.facebook.AccessToken;
 import com.facebook.login.LoginManager;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomBar mBottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,28 @@ public class MainActivity extends AppCompatActivity {
         if (AccessToken.getCurrentAccessToken() == null) {
             goLoginScreen();
         }
+
+        mBottomBar = BottomBar.attach(this, savedInstanceState);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame, new ClubsFragment()).commit();
+        mBottomBar.setItemsFromMenu(R.menu.menu_icons, new OnMenuTabSelectedListener() {
+
+            @Override
+            public void onMenuItemSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.BottomMenuIconOneHome) {
+                    ClubsFragment f = new ClubsFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame, f).commit();
+                } else if (menuItemId == R.id.BottomMenuIconTwoSearch) {
+                    SearchFragment f = new SearchFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame, f).commit();
+                } else if (menuItemId == R.id.BottomMenuIconThreeChat) {
+                    ChatFragment f = new ChatFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame, f).commit();
+                } else if (menuItemId == R.id.BottomMenuIconFourAccount) {
+                    AccountFragment f = new AccountFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame, f).commit();
+                }
+            }
+        });
     }
 
     private void goLoginScreen() {
